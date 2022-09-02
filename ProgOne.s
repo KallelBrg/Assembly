610679ea56f2641e8b207064c5be5c119e16b67a
Kallel Braga Bezerra <107359212+KallelBrg@users.noreply.github.com>
2022-09-02 08:22:15 -0300

    Update ProgOne.s

--- ProgOne.s
+++ ProgOne.s
@@ -10,7 +10,7 @@ _start:
     
     mov edx, len ;quantidade de elementos
     mov ecx, mens ; ponteiro da string 
-    mov ebx, 1
+    mov ebx, 1 ;descritor de arquivo que manda para a tela(pode mandar para outros lugares alem da tela diferente 1)
     mov eax, 4 ;monitor
     int 80h   ;chama kernel
     
