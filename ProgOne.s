5af4a70ac951eb0e119ca74845fd0518892950e4
Kallel Braga Bezerra <107359212+KallelBrg@users.noreply.github.com>
2022-09-09 08:02:26 -0300

    Update ProgOne.s

--- ProgOne.s
+++ ProgOne.s
@@ -8,12 +8,16 @@ global _start
 
 _start:
     
-    mov edx, len ;quantidade de elementos
-    mov ecx, mens ; ponteiro da string 
-    mov ebx, 1 ;descritor de arquivo que manda para a tela(pode mandar para outros lugares alem da tela diferente 1)
-    mov eax, 4 ;monitor
-    int 80h   ;chama kernel
+    mov edx, len ;quantidade de elementos / operando imediato
+    mov ecx, mens ; ponteiro da string  / operando imediato
+    mov ebx, 1 ;descritor de arquivo que manda para a tela(pode mandar para outros lugares alem da tela diferente 1) / operando imediato
+    mov eax, 4 ;monitor / operando imediato
+    int 80h   ;chama kernel / operando imediato
     
     mov eax, 1 ; EXIT
     mov ebx, 0 ; return
     int 80h ; encerra
+    
+    ;nasm -f elf64 nada.s
+    ;ld nada.o -o nada
+    ;./nada
