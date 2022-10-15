5c30171c9a6a35d6edbda37518d89f8c59c04c48
Kallel Braga Bezerra <107359212+KallelBrg@users.noreply.github.com>
2022-10-15 16:59:44 -0300

    Update ProgFour.s

--- ProgFour.s
+++ ProgFour.s
@@ -14,6 +14,7 @@ section .text
 
 
 _start:
+    ;exibe mensagem para entrar com uma frase
     mov eax, 4
     mov ebx, 1
     mov ecx, mens
@@ -25,16 +26,17 @@ _start:
     mov ecx, b1
     mov edx, 500
     int 80h ; vai retornar o tamanho da escrita do teclado em eax
-    mov esi, 0
+    mov esi, 0 ;index para varrer a string
     
 iniloop:
     mov al,[b1+esi]
     mov [b2+esi],al
-    cmp al, 10
-    je sai
-    inc esi
-    jmp iniloop
+    cmp al, 10 ;compara para ver se o index já chegou no enter ou nao
+    je sai ;se for igual pula para fora do loop
+    inc esi ;incrementa o index
+    jmp iniloop ;volta para o loop
 sai:
+    ;imprime a mensagem que foi transferia de b1 para b2
     mov eax, 4
     mov ebx, 1
     mov ecx, b2
@@ -42,6 +44,7 @@ sai:
     mov edx, esi
     int 80h
     
+    ;fim
     mov eax, 1
     mov ebx, 0
     int 80h
