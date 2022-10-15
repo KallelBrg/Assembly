d783272e8d8fec300eac5aa4a209240f1934991d
Kallel Braga Bezerra <107359212+KallelBrg@users.noreply.github.com>
2022-10-15 16:57:30 -0300

    Update ProgThree.s

--- ProgThree.s
+++ ProgThree.s
@@ -37,7 +37,7 @@ _start:
     mov eax, 3
     int 80h
     
-    cmp eax, 1 
+    cmp eax, 1 ;se a msg digitada for vazia, o prog volta para digitar de novo
     je _start
     
     ;Exibir mensagem 
@@ -47,16 +47,17 @@ _start:
     mov eax, 4
     int 80h
     
+    ; exibe a mensagem digitada
     mov eax, 4
     mov ebx, 1
     mov ecx, mentr
     mov edx, 30
     int 80h
     
-    cmp [mentr+0], byte "u"
-    je sezim
+    cmp [mentr+0], byte "u" ; verifica se o primeiro char da string é u
+    je sezim ;se for u ele pula pro label sezim e diz que tem u
     
-    ;Exibir mensagem 
+    ;Exibir mensagem que não tem u
     mov edx, siz
     mov ecx, sa
     mov ebx, 1
@@ -66,7 +67,7 @@ _start:
     jmp final
         
 sezim:
-    ;Exibir mensagem 
+    ;Exibir mensagem digitada
     mov edx, size
     mov ecx, sai
     mov ebx, 1
