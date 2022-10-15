bc720ddf8128d87674b5f6d0ef4168c51b38be25
Kallel Braga Bezerra <107359212+KallelBrg@users.noreply.github.com>
2022-10-15 16:51:11 -0300

    Update ProgTwo.s

--- ProgTwo.s
+++ ProgTwo.s
@@ -15,24 +15,28 @@ segment .text
     
 _start:
     
+    ;imprime a msg pra entrar com a frase
     mov edx, tamor
     mov ecx, msor
     mov ebx, 1
     mov eax, 4
     int 80h
     
+    ;recebe a string digitada do teclado
     mov edx, 30
     mov ecx, mentr
     mov ebx, 0
     mov eax, 3
     int 80h
     
+    ;imprime uma msg falando qual é a msg digitada
     mov edx, len
     mov ecx, saida
     mov ebx, 1
     mov eax, 4
     int 80h
     
+    ;immprime a msg digitada
     mov eax, 4
     mov ebx, 1
     mov ecx, mentr
